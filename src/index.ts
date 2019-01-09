994c63bcb62528d1de09dbcf780d8eec2618d293
Aaron Scherer <aequasi@gmail.com>
2019-01-09 00:48:53 -0800

    fix(index): Adding manage channels perm for invite, used to make sure the bot has perms to post in setup channel

--- src/index.ts
+++ src/index.ts
@@ -151,7 +151,7 @@ export default class extends AbstractPlugin {
     @Decorator.Command('invite', 'Outputs an invite url for this bot.')
     public async InviteCommand(): Promise<void> {
         // tslint:disable-next-line
-        return this.reply(`https://discordapp.com/api/oauth2/authorize?client_id=305140278480863233&permissions=93248&scope=bot`)
+        return this.reply(`https://discordapp.com/api/oauth2/authorize?client_id=305140278480863233&permissions=92240&scope=bot`)
     }
 
     @Decorator.Command('stats', 'Gets information about the bot')
